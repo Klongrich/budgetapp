25f793a2f6975ebb8ae28de8c000be6d4bb627f4
klongrich <longrichk@gmail.com>
2026-06-02 09:36:33 -0400

    adding javascript to get user input then display it's value

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//Setting current date
 const today = new Date().toLocaleDateString();
 
 document.getElementById('current-date').textContent = today;
+
+//Getting user input
+const form = document.getElementById('form');
+const priceInput = document.getElementById('price');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  document.getElementById('display-amount').textContent = priceInput.value;
+});
