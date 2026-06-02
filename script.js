7005f87bac8c63045e4045b9a0ac9cf8ee9a7219
klongrich <longrichk@gmail.com>
2026-06-02 10:44:38 -0400

    adding 3 values to page from user

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ document.getElementById('current-date').textContent = today;
 //Getting user input
 const form = document.getElementById('form');
 const priceInput = document.getElementById('price');
+const amount = document.getElementById('price');
+const loc = document.getElementById('location');
+const iteam = document.getElementById('iteam');
+
 let total = 0;
 
 form.addEventListener('submit', function(event) {
@@ -13,6 +17,11 @@ form.addEventListener('submit', function(event) {
   
   total += Number(priceInput.value);
   document.getElementById('display-amount').textContent = total;
+
+  document.getElementById('location-1').textContent = loc.value;
+  document.getElementById('iteam-1').textContent = iteam.value;
+  document.getElementById('amount-1').textContent = amount.value;
+  
   localStorage.setItem('Amount-Spent', total);
 });
 
@@ -20,7 +29,9 @@ const resetButton = document.getElementById('reset');
 
 resetButton.addEventListener('click', function() {
 	total = 0;
+	
 	localStorage.setItem('Amount-Spent', 0);
+	
 	document.getElementById('display-amount').textContent = total;
 });
 
