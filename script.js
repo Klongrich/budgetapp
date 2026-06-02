72256285d1e5d6c25bcc9d014e9784438d2c85c2
klongrich <longrichk@gmail.com>
2026-06-02 10:14:43 -0400

    adding cacheing to store budget data

--- script.js
+++ script.js
@@ -13,11 +13,23 @@ form.addEventListener('submit', function(event) {
   
   total += Number(priceInput.value);
   document.getElementById('display-amount').textContent = total;
+  localStorage.setItem('Amount-Spent', total);
 });
 
 const resetButton = document.getElementById('reset');
 
 resetButton.addEventListener('click', function() {
 	total = 0;
+	localStorage.setItem('Amount-Spent', 0);
 	document.getElementById('display-amount').textContent = total;
 });
+
+function initializeApp() {
+	let amount_spent = localStorage.getItem('Amount-Spent');
+	total = Number(amount_spent);
+	document.getElementById('display-amount').textContent = amount_spent;
+	console.log("The webpage has fully loaded!");
+}
+
+// 2. Trigger it automatically when the HTML structure is complete
+window.addEventListener('DOMContentLoaded', initializeApp);
